cfe563af96ce89dd881ab95933f8eb2bf5f0e460
TheNotary <no@email.plz>
2016-08-11 19:21:47 -0500

    code is complete and working

--- tech_challenge.rb
+++ tech_challenge.rb
@@ -3,15 +3,17 @@
 #   - treat submitted code as though it has production value
 require 'pry'
 
-watsons_string = "aabbc"
 
 
 module StringCorrector
 
+  # Ignore me, I'm some of Sherlock's code
   # takes in a string an corrects it such that each distinct
   # character occurse exactly as often as each other, eg "aabbc"
   # becomes
   def correct_string(string)
+    raise "Not Implemented!"
+
     ensure_string_is_solvable!(string)
 
     return string if string_valid?(character_by_frequency_hash)
@@ -27,9 +29,18 @@ module StringCorrector
       character_by_frequency_hash = construct_frequency_analysis_hash(string)
 
       # TODO: finish this code just for fun
-      most_frequent_char = character_by_frequency_hash.last.key
+      most_frequent_char = character_by_frequency_hash.values.last
     end
   end
+
+  # Ignore me, I'm some of Sherlock's code
+  # the string is valid if all letters have the same frequency count, i.e.
+  # there is 1 frequency throughout the letter/ frequency hash
+  def string_valid?(character_by_frequency_hash)
+    character_by_frequency_hash.values.uniq == 1
+  end
+
+
 
   # raises an exception if the string does not meet the minimum criteria to be
   # solved, e.g.
@@ -42,31 +53,25 @@ module StringCorrector
       raise "ERROR: the string is less than or equal to 1 character"
     elsif string.length > 100_000 # 10**5
       raise "ERROR: the string is too large"
-    elsif string =~ character_validation_pattern
+    elsif (string =~ character_validation_pattern).nil?
       raise "ERROR: the string contained characters other than those [a-z]"
     end
   end
 
-  # the string is valid if all letters have the same frequency count, i.e.
-  # there is 1 frequency throughout the letter/ frequency hash
-  def string_valid?(character_by_frequency_hash)
-    character_by_frequency_hash.values.uniq == 1
-  end
-
-  # string = "abcddd"
-
+  # outputs a hash depicting the frequencey of occurance for each distinct
+  # character in the string
   def construct_frequency_analysis_hash(string)
     string.split("").each_with_object(Hash.new(0)) do |letter, hash|
       hash[letter] += 1 # increment frequency count for this letter
     end
   end
 
-  # Code for contractor
+  # Returns the count of the number of characters that need to be removed
+  # from the string to make it valid.
   def deduce_number_of_removals_required_to_correct_a_string(string)
     character_by_frequency_hash = construct_frequency_analysis_hash(string)
 
-    binding.pry
-    most_infrequent_count = character_by_frequency_hash.values.uniq.first
+    most_infrequent_count = character_by_frequency_hash.values.sort.uniq.first
 
     character_by_frequency_hash.inject(0) do |total_removals_required, k_v|
       letter, count = k_v
@@ -90,7 +95,8 @@ end
 # approach to the problem.
 class ContractorToSherlock
 
-  # TODO:  consider mixin of StringCorrector...
+  # meta-notes: There's shared functionality between what the contractor to
+  # sherlock does and what sherlock himself does :)
   include StringCorrector
 
   # Sherlock will call this method to determine how 'easy'
@@ -102,21 +108,26 @@ class ContractorToSherlock
   #   2) the algorithm is straigh forward enough to readily see via well
   #      thoughtout logic abstraction
   def validate_string_for_sherlock(string)
+    ensure_string_is_solvable!(string)
+
     if solvable_in_one_removal?(string)
-      return "YES"
+      "YES"
     else
-      return "NO"
+      "NO"
     end
   end
 
-  def solvable_in_one_removal?(string)
-    if deduce_number_of_removals_required_to_correct_a_string(string) <= 1
-      true
-    else
-      false
-    end
-  end
 
+  private
+    # meta-notes: I gave this it's own function to hide the conditional check
+    # which so the top-most algo looks absolutely crystal clear.
+    def solvable_in_one_removal?(string)
+      if deduce_number_of_removals_required_to_correct_a_string(string) <= 1
+        true
+      else
+        false
+      end
+    end
 end
 
 
@@ -124,4 +135,16 @@ end
 # about a strings that watson gives to him.
 ben = ContractorToSherlock.new
 
+# watsons_string = "aabbc"
+watsons_string = ARGF.read
+
 puts ben.validate_string_for_sherlock(watsons_string)
+
+
+puts ben.validate_string_for_sherlock("abc")
+puts ben.validate_string_for_sherlock("abzzc")
+
+# puts ben.validate_string_for_sherlock("")
+
+# puts ben.validate_string_for_sherlock("a" + "a" * 10**5)
+# puts ben.validate_string_for_sherlock("abzzC")
